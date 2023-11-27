f059a1ae61bc3d8cad105962a2280d00db79e5c5
SneyderRubio <esneiderrubio3107@gmail.com>
2023-11-27 18:11:36 -0500

    Update
    acomodo de video

--- src/app/cartillainit/cartillainit.component.ts
+++ src/app/cartillainit/cartillainit.component.ts
@@ -23,13 +23,15 @@ export class CartillainitComponent {
       imageUrl: 'assets/gif/phishing-phisher.gif',
       title: 'PHISHING',
       description1: 'Información: El phishing es un ataque en el que los delincuentes intentan engañar a las personas para que revelen información confidencial, como contraseñas o información financiera, a menudo a través de correos electrónicos, mensajes de texto o llamadas telefónicas.',
-      description2: 'Ejemplo: Mostrar ejemplos de correos electrónicos de phishing que simulan ser de instituciones bancarias o proveedores de servicios y solicitan información personal.'
+      description2: 'Ejemplo: Mostrar ejemplos de correos electrónicos de phishing que simulan ser de instituciones bancarias o proveedores de servicios y solicitan información personal.',
+      videoUrl:'https://youtu.be/M2HaMR3H0Cg?si=QqDTVWOJ7kyEBZ3q'
     },
     {
       imageUrl: 'assets/gif/ciberseguridad.gif',
       title: 'xd',
       description1: 'como es',
-      description2: 'por que'
+      description2: 'por que',
+      videoUrl:'https://www.youtube.com/watch?v=M2HaMR3H0Cg&ab_channel=SBSPer%C3%BA'
     },
 
     // Agrega más elementos según sea necesario
